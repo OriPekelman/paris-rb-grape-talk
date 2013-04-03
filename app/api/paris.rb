141f81e63b25e2cdb286874065fb84428e636b90
Ori Pekelman <ori@pekelman.com>
2013-04-03 13:45:21 +0200

    Let's show some grape goodness and add versioning while defaulting to json

--- app/api/paris.rb
+++ app/api/paris.rb
@@ -1,5 +1,8 @@
 module Paris
   class RB < Grape::API
+    version 'v1', :using => :path, :vendor => 'parisrb'
+    format :json
+    
     desc "Root of api"
     get "/" do
       {:link=>"Home sweet home"}
