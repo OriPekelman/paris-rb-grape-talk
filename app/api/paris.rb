e4d8bc32b7367fcf6ca437f0eb582c7447195d33
Ori Pekelman <ori@pekelman.com>
2013-04-03 13:49:31 +0200

    Ok, our own custom API logic.

--- app/api/paris.rb
+++ app/api/paris.rb
@@ -3,9 +3,20 @@ module Paris
     version 'v1', :using => :path, :vendor => 'parisrb'
     format :json
     
-    desc "Root of api"
-    get "/" do
-      {:link=>"Home sweet home"}
-    end
+    resource :talks do  
+      desc "Return the collection of current talks"
+      get :current do
+        Talk.limit(20)
+      end
+
+      desc "Return a talk."
+      params do
+        requires :id, :type => String, :desc => "talk id."
+      end
+      get ':id' do
+        Talk.find(params[:id])
+      end
+    end     
+
   end
 end
